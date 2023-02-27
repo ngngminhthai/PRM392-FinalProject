9e8fb709a6f1c98d189b56dbb17e2cf19d2c9245
ntvinh4@cmc.com.vn <neulaanh15@gmail.com>
2023-02-27 23:51:11 +0700

    Newfeed

---
+++ app/src/main/java/com/example/prm392project/data/model/ui/newfeed/NewFeedViewModel.java
@@ -0,0 +1,7 @@
+package com.example.prm392project.data.model.ui.newfeed;
+
+import androidx.lifecycle.ViewModel;
+
+public class NewFeedViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
