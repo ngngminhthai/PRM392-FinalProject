37b67e1ff8cac8b73a5c1df3847a0e5d676115b2
Tuan Thanh <tuanthanhnghean123@gmail.com>
2023-02-28 00:15:42 +0700

    first notification ui

---
+++ app/src/main/java/com/example/prm392project/ui/notification/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.prm392project.ui.notification.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
