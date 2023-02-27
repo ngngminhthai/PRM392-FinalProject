e4de4497b807021d9e7ed3485a9e42ebcd2918b3
nguyenminhthaijs <thainguyenminh441@gmail.com>
2023-02-27 23:12:48 +0700

    Login Activity

--- app/src/main/java/com/example/prm392project/MainActivity.java
+++ app/src/main/java/com/example/prm392project/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.prm392project.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
         setContentView(R.layout.activity_main);
     }
 }
